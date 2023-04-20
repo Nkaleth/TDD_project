4b5c4572182a24653f28c7f6e2af5d4b2a6de204
Dennis Every <dennis@ansyra.com>
2023-04-20 16:21:05 -0400

    Add spec for #factorial with arg = 0 to return 1

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,10 +3,14 @@ require './solver'
 
 describe Solver do
   describe '#factorial' do
-    it 'takes an argument' do
+    it 'takes an integer as an argument' do
+      # Arrange
       s = Solver.new
       arg = 0
-      s.factorial(arg)
+      # Act
+      result = s.factorial(arg)
+      # Assert
+      expect(result).to eq(1)
     end
   end
 end
