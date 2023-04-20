55d2240fa0b2dd7d8925105f68401fb950d837a7
Dennis Every <dennis@ansyra.com>
2023-04-20 16:13:12 -0400

    Add describe #factorial takes an argument

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,4 +2,11 @@ require 'rspec'
 require './solver'
 
 describe Solver do
+  describe '#factorial' do
+    it 'takes an argument' do
+      s = Solver.new
+      arg = 0
+      Solver.factorial(arg)
+    end
+  end
 end
