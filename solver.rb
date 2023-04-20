7029be96a830ef9db0c8659a5f92778fd18bdecb
Dennis Every <dennis@ansyra.com>
2023-04-20 16:50:18 -0400

    Add #factorial method calculation

--- solver.rb
+++ solver.rb
@@ -2,5 +2,6 @@ class Solver
   def factorial(arg)
     raise ArgumentError, "#{self.class} '#{__method__}' argument cannot be negative" if arg.negative?
     return 1 if arg.zero?
+    (1..arg).inject(1) { |product, i| product * i }
   end
 end
